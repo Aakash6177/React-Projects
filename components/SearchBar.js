ea4fd827f5b0ff46e3bc7829eaf378a05216eee2
Aakash6177 <www.aakash7469@gmail.com>
2022-10-29 16:21:04 -0700

    added a external module

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,32 +1,21 @@
 import React from 'react'
 import {View, Text} from 'react-native'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
-import Icon from 'react-native-ionicons'
-import AntDesign from  'react-native-vector-icons/AntDesign'
+
 
 export default function SearchBar(){
 
 
     return (
-        <View style={{marginTop: 15, flexDirection: "row"}}>
-               <GooglePlacesAutocomplete
-      ref={ref}
-      placeholder='Search'
-      onPress={(data, details = null) => {
-        // 'details' is provided when fetchDetails = true
-        console.log(data, details);
-      }}
-      query={{
-        key: 'YOUR API KEY',
-        language: 'en',
-      }}
+        <View style={{marginTop: 15, flexDirection: "row"}}>1
+               <GooglePlacesAutocomplete placeholder = "Search"
+     
    
-            
-            
+
 
             renderLeftButton = {() => {
                 <View>
-                    <Icon name = 'location-sharp' size = {24}/>
+      
                 </View>
             }}
 
@@ -35,7 +24,7 @@ export default function SearchBar(){
                     flexDirection: "row",
 
                 }}>
-                    <AntDesign name= "clockcircle" size={11}/>
+              
                     <Text>Search</Text>
                 </View>
             }}
